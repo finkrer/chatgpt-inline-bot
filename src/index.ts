9893b6b362f12395516e2f2f1100eda703dcf371
finkrer <finkrer@gmail.com>
2023-07-10 05:49:56 +0500

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,64 @@
+import { Telegraf } from 'telegraf';
+import createDebug from 'debug';
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import { development, production } from './core';
+import { nanoid } from 'nanoid';
+import { getSingleCompletion } from './openai/api';
+
+const BOT_TOKEN = process.env.BOT_TOKEN || '';
+const ENVIRONMENT = process.env.NODE_ENV || '';
+
+const bot = new Telegraf(BOT_TOKEN);
+
+const debug = createDebug('bot:inline_query');
+
+bot.on('inline_query', async (ctx) => {
+  debug('Triggered inline query');
+
+  const query = ctx.inlineQuery.query;
+
+  if (!query.endsWith('?')) {
+    return;
+  }
+
+  let chatGptAnswer: string;
+  try {
+    chatGptAnswer = await getSingleCompletion(query);
+  } catch {
+    return;
+  }
+
+  try {
+    return await ctx.answerInlineQuery([
+      {
+        type: 'article',
+        id: nanoid(),
+        title: 'Conversation',
+        description: `${ctx.inlineQuery.query}\n\n${chatGptAnswer}`,
+        input_message_content: {
+          message_text: `<b>❓ ${ctx.inlineQuery.query}</b>\n\n🤖 ${chatGptAnswer}`,
+          parse_mode: 'HTML',
+        },
+      },
+      {
+        type: 'article',
+        id: nanoid(),
+        title: 'Answer',
+        description: chatGptAnswer,
+        input_message_content: {
+          message_text: chatGptAnswer,
+          parse_mode: 'HTML',
+        },
+      },
+    ]);
+  } catch {
+    return;
+  }
+});
+
+//prod mode (Vercel)
+export const startVercel = async (req: VercelRequest, res: VercelResponse) => {
+  await production(req, res, bot);
+};
+//dev mode
+ENVIRONMENT !== 'production' && development(bot);
---
+++ src/openai/api.ts
@@ -0,0 +1,26 @@
+import { Configuration, OpenAIApi } from 'openai';
+import createDebug from 'debug';
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const debug = createDebug('bot:inline_query');
+
+export const getSingleCompletion = async (prompt: string) => {
+  debug(`triggered completion with prompt: ${prompt}`);
+  return await openai
+    .createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      temperature: 0.7,
+      messages: [
+        {
+          role: 'system',
+          content: `You are a question answering bot for the Telegram messenger. People call you with an inline query in their message and you answer the question right in the chat. The answer must be concise and to the point and contain only the facts requested. Don't address the user, don't ask further questions, don't repeat the words from the question. It's ok to start the answer with "because" or just list the requested facts with no additional words.`,
+        },
+        { role: 'user', content: prompt },
+      ],
+    })
+    .then((data) => data.data.choices[0].message?.content as string);
+};
